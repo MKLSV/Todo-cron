0479f62571a6ef2fcc0fa388a5d106bcd26129d7
Matvey Kolosov <118886359+MKLSV@users.noreply.github.com>
2024-12-16 14:14:22 +0300

    Add files via upload

---
+++ api/cron.js
@@ -0,0 +1,86 @@
+
+import { getDataFromMongoDB, sendToTelegram } from '../service.js';
+
+export default async function handler(req, res) {
+  try {
+    const response = await getDataFromMongoDB()
+
+    const time = Date.now()
+    const filteredData = response.filter(val => !val.isCompleted && val.dueDate && val.dueDate - time < (3 * 24 * 60 * 60 * 1000))
+    const DataForAll = filteredData.filter(val => val.owner === 'Для всех')
+    const DataForMT = filteredData.filter(val => val.owner === 'Матвей')
+    const DataForDea = filteredData.filter(val => val.owner === 'Делайла')
+
+    const MTtasks = [...DataForAll, ...DataForMT]
+    const DEAtasks = [...DataForAll, ...DataForDea]
+    sendMsg(MTtasks, "MT")
+    sendMsg(DEAtasks, "DEA")
+
+    res.status(200).json({ message: 'Сообщение отправлено' });
+  } catch (error) {
+    console.error('Ошибка:', error);
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
+}
+
+// async function getData() {
+
+// }
+
+// Функция для отправки сообщений в Telegram
+async function sendMsg(tasks, user) {
+  const sortedTasks = sortTasks(tasks)
+  await sendToTelegram('Привет, вот задачи которые нужно выполнить в ближайшие дни:', user)
+  if (sortedTasks.overdue.length) {
+    const msg = '🕒Просрочено:\n' + sortedTasks.overdue.map(task => {
+      return '📌' + task.text + '\n'
+    }).join('')
+    await sendToTelegram(msg, user)
+  }
+  if (sortedTasks.today.length) {
+    const msg = '🕒Сегодня:\n' + sortedTasks.today.map(task => {
+      return '📌' + task.text + '\n'
+    }).join('')
+    await sendToTelegram(msg, user)
+  }
+  if (sortedTasks.tommorow.length) {
+    const msg = '🕒До Завтра:\n' + sortedTasks.tommorow.map(task => {
+      return '📌' + task.text + '\n'
+    }).join('')
+    await sendToTelegram(msg, user)
+  }
+  if (sortedTasks.afterTomorrow.length) {
+    const msg = '🕒До Послезавтра:\n' + sortedTasks.afterTomorrow.map(task => {
+      return '📌' + task.text + '\n'
+    }).join('')
+    await sendToTelegram(msg, user)
+  }
+
+}
+
+
+function sortTasks(tasks) {
+  const today = new Date(); // Текущая дата
+  const todayDate = today.getDate()
+  const todayMonth = today.getDate()
+  let sortedTasks = {
+    overdue: [],
+    today: [],
+    tommorow: [],
+    afterTomorrow: [],
+  }
+  tasks.forEach(task => {
+    const taskDate = new Date(task.dueDate); // Преобразуем в объект Date
+    if (taskDate.getDate() == todayDate) sortedTasks.today.push(task)
+    else if (taskDate.getDate() - todayDate === 1 || (taskDate.getMonth() > todayMonth) && taskDate.getDate() === 1) sortedTasks.tommorow.push(task)
+    else if (taskDate.getDate() - todayDate === 2 || (taskDate.getMonth() > todayMonth) && taskDate.getDate() === 2) sortedTasks.afterTomorrow.push(task)
+    else if (taskDate < today) sortedTasks.overdue.push(task)
+  })
+
+  return sortedTasks
+}
+
+
+
+
+
