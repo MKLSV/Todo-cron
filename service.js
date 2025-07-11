e37e90bd1b3c848d9421f3cc51ec455bcf6e473c
MKLSV <mkolosov96@gmail.com>
2025-07-11 13:11:37 +0300

    a

--- service.js
+++ service.js
@@ -8,6 +8,31 @@ const TELEGRAM_CHAT_DEA = "644190724"
 
 const bot = new TelegramBot(token, { polling: true });
 
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                ]
+            }
+        })
+
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
+});
+
+
 export async function getDataFromMongoDB() {
   const dbName = 'TodoDB';
   const collectionName = 'tasks';
@@ -50,26 +75,3 @@ export async function sendToTelegram(message, user) {
   }
 }
 
-
-bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
-
-    if(text === '/start') {
-        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
-            reply_markup: {
-                keyboard: [
-                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
-                ]
-            }
-        })
-
-        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
-            reply_markup: {
-                inline_keyboard: [
-                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
-                ]
-            }
-        })
-    }
-});
